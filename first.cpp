25d8c0dbd116659fc1683fb4b3551d374ad448e2
Prashant-9253 <161339138+Prashant-9253@users.noreply.github.com>
2024-02-26 21:00:47 +0530

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"Prashant Raj------->21BCS9243"<<ensl;
+cout<<"enter two numbers to add"<<endl;
+cin>>a>>b;
+c=a+b;
+cout<<"The sum of "<<a<<"and"<<b<<"is"<<c<<endl;
+return 0;
+}
